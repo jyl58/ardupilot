c2b5afecc99bda0f754a487fb3984ac6ee84ef7b
jiyingliang <jiyingliang369@126.com>
2018-04-27 10:15:25 +0800

    将gazebo 运行在后台

--- Tools/autotest/sim_formation.sh
+++ Tools/autotest/sim_formation.sh
@@ -9,7 +9,7 @@ tcp_port=$((5760+(10*$1)))
 sitl_port=$((5501+(10*$1)))
 
 # first setup gazebo at back
-gazebo  $3 
+gazebo  $3 &
 
 # second setup arducopter bin at back
 ../build/sitl/bin/arducopter -S -I$1 --home -35.363261,149.165230,584,353 --model gazebo-iris --speedup 1 --defaults ../Tools/autotest/default_params/copter.parm,../Tools/autotest/default_params/gazebo-iris.parm  & 
