18834eaa09b0493087431ff91f3dcdd25d13a5af
jiyingliang <jiyingliang369@126.com>
2018-12-11 15:22:46 +0800

    修改错误函数名

--- ArduCopter/mode_guided.cpp
+++ ArduCopter/mode_guided.cpp
@@ -182,7 +182,7 @@ void Copter::ModeGuided::loiter_control_start()
 
     // initialize vertical speed and acceleration
     pos_control->set_max_speed_z(-get_pilot_speed_dn(), g.pilot_speed_up);
-    pos_control->set_maxaccel_z(g.pilot_accel_z);
+    pos_control->set_max_accel_z(g.pilot_accel_z);
 
     // initialise position and desired velocity
     if (!pos_control->is_active_z()) {
