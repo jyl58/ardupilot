1bfa72bd51b613f82ec61ca42fbdf3b136a1746b
jiyingliang <jiyingliang369@126.com>
2020-06-25 20:10:34 +0800

    添加DYP解析数据

--- libraries/AP_Proximity/AP_Proximity_DYP.cpp
+++ libraries/AP_Proximity/AP_Proximity_DYP.cpp
@@ -36,10 +36,71 @@ AP_Proximity_DYP::update()
         return;
     }
     //TODO: add the read uart code
-
+    int16_t nbytes = _uart->available();
+    while (nbytes-- > 0) {
+        const int16_t r = _uart->read();
+        if ((r < 0) || (r > 0xFF)) {
+            continue;
+        }
+        collect_byte((uint8_t)r);
+    }
     if ((_last_distance_received_ms == 0) || ((AP_HAL::millis() - _last_distance_received_ms) > PROXIMITY_DYP_TIMEOUT_MS)) {
         set_status(AP_Proximity::Status::NoData);
     } else {
         set_status(AP_Proximity::Status::Good);
+    }
+}
+void 
+AP_Proximity_DYP::collect_byte(uint8_t msg)
+{
+    switch(_parse_phase){
+        case  STEP_head:
+            if(msg==0XFF){
+                _parse_phase=STEP_data;
+                _data_index=0;
+            }
+            break;
+        case STEP_data:
+            _data[_data_index]=msg;
+            _data_index++;
+            if(_data_index>=8){
+                parserMsg();
+                _parse_phase=STEP_data;
+            }
+            break;
+        default :
+        break;
+    }
+}
+void
+AP_Proximity_DYP::parserMsg()
+{
+    uint16_t check_sum=0XFF;
+    for(int i=0;i<8;i++){
+        check_sum+=_data[i];
+    }
+    uint8_t low_check_sum=(uint8_t) check_sum&0xFF;
+    if(low_check_sum==_data[8]){
+        uint32_t now = AP_HAL::millis();
+        _last_distance_received_ms= now;
+        _angle[0] = 315;
+        _distance[0] = (_data[0]*256+_data[1])*0.001;//mm-->m
+        _distance_valid[0] = (_distance[0] >= 0.3) && (_distance[0] <= 4.5);
+        update_boundary_for_sector(0, true);
+            
+        _angle[1] = 0;
+        _distance[1] = (_data[2]*256+_data[3])*0.001;//mm-->m
+        _distance_valid[1] = (_distance[1] >= 0.3) && (_distance[1] <= 4.5);
+        update_boundary_for_sector(1, true);
+    
+        _angle[2] = 45;
+        _distance[2] = (_data[4]*256+_data[5])*0.001;//mm-->m
+        _distance_valid[2] = (_distance[2] >= 0.3) && (_distance[2] <= 4.5);
+        update_boundary_for_sector(2, true);
+
+        _angle[3] = 0;
+        _distance[3] = (_data[6]*256+_data[7])*0.001;//mm-->m
+        _distance_valid[3] = (_distance[3] >= 0.3) && (_distance[3] <= 4.5);
+        update_boundary_for_sector(3, true);
     }
 }
--- libraries/AP_Proximity/AP_Proximity_DYP.h
+++ libraries/AP_Proximity/AP_Proximity_DYP.h
@@ -13,9 +13,18 @@ class AP_Proximity_DYP:public AP_Proximity_Backend
          // get maximum and minimum distances (in meters) of sensor
         float distance_max() const override { return 4.5f; }
         float distance_min() const override { return 0.30f; }
+        enum PARSE_phase{
+            STEP_head=0,
+            STEP_data
+        };
     private:
         void initialise();
+        void collect_byte(uint8_t msg);
+        void parserMsg();
+        enum PARSE_phase _parse_phase{STEP_head};
         uint32_t _last_distance_received_ms;    // system time of last distance measurement received from sensor
         AP_HAL::UARTDriver *_uart=nullptr;              // uart for communicating with sensor
+        uint8_t _data[9];
+        uint8_t _data_index=0;
 
 };
